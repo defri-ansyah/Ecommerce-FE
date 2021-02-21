eb1f10d5e88c2dde5546ac7454e33ff5fad187f8
Defri Ansyah <defriansyah013@gmail.com>
2021-02-22 03:36:49 +0700

    add modal on search

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ import '../assets/Header.css'
 class Header extends Component {
   render() {
     return (
+      <div>
       <div className='header'>
         <div className='item-header-1 d-flex justify-content-between align-items-center'>
           <div className='d-flex align-items-center'>
@@ -24,7 +25,7 @@ class Header extends Component {
             <img className='img-tokopedia' src="img/2.png" alt=""/>
             <span className='mx-2 text-kategori'>Kategori</span>
             <div className='wrap-search'>
-              <input type="text" className='form-control' placeholder='Cari barang'/>
+              <input type="text" className='form-control' placeholder='Cari barang' data-bs-toggle="modal" data-bs-target="#exampleModal"/>
               <div className='wrap-icon-search'>
                 <img className='img-search' src="img/3.png" alt=""/>
               </div>
@@ -42,8 +43,28 @@ class Header extends Component {
             </div>
           </div>
         </div>
-
       </div>
+      {/* Modal */}
+      <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div className="modal-dialog">
+        <div className="modal-content m-c-head">
+          <div className='d-flex justify-content-between'>
+            <span className='font-weight-bold title'>Pencarian Terakhir</span>
+            <span className='font-weight-bold' style={{color: '#d50000'}}>Hapus Semua</span>
+          </div>
+          <span className='ms-2 mt-2' style={{fontSize: '14px'}}>Celana Panjang</span>
+          <span className='ms-2 mt-2' style={{fontSize: '14px'}}>Sepatu Pria</span>
+          <span className='ms-2 mt-2' style={{fontSize: '14px'}}>Baju Kaos</span>
+          <span className='font-weight-bold mt-3 mb-2 title'>Populer</span>
+          <div className='d-flex flex-wrap'>
+            <button className='btn me-2'>Sepatu Bola</button>
+            <button className='btn me-2'>Baju Renang</button>
+            <button className='btn me-2'>Seragam Sekolah</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
     )
   }
 }
